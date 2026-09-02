a2b78fb64fee54ff3e83b69288c9a2fd58ae19e0
Abhishek Kumar <abhishek.kumar@celebigo.com>
2026-09-02 16:40:32 +0530

    some chaanges

--- tests/Feature/ExampleTest.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-test('guests are redirected to the login page', function () {
-    $response = $this->get('/');
-
-    $response->assertRedirect('/dashboard');
-
-    $this->get('/dashboard')->assertRedirect('/login');
-});
